29a233a7d71c8a82f1e1b32de91b1bad14d3caa7
KhanhGN <71396471+KhanhGN@users.noreply.github.com>
2021-10-20 09:20:32 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,13 @@
+  var btn = document.querySelector('.burger-menu');
+  var list = document.querySelector('.nav-list');
+  var btnOpen = false;
+  btn.addEventListener('click', function(T){
+    if (btnOpen) {
+      list.classList.remove('open');
+      btnOpen = false;
+    }
+    else {
+      list.classList.add('open');
+      btnOpen = true;
+    }
+  });
