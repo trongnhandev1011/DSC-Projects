dfc2992411a186a6912b0d2bc727de375ad89555
KhanhGN <71396471+KhanhGN@users.noreply.github.com>
2021-10-27 13:10:19 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
-topDropDown();
-addEmptyList();
-
-function addEmptyList() {
-  var itemCount = 0;
+function addEmptyList(itemCount) {
   const cartList = document.querySelector('.cart-list');
+  // If 0 item was loaded for the current user, add an empty <li> saying "Add something to your cart"
   if (itemCount == 0) {
     var emptyText = document.createTextNode('Add something to your cart');
     var emptyList = document.createElement('li');
@@ -11,25 +8,34 @@ function addEmptyList() {
     emptyList.appendChild(emptyText);
     cartList.appendChild(emptyList);
   } else {
+    // If some items were loaded, or the user select an item, remove the empty item
     document.querySelector('.empty-item').remove();
   }
 }
 
 function topDropDown() {
+  // Get all of the required elements
   const burger = document.querySelector('.burger-menu');
   const burgerList = document.querySelector('.nav-list');
   const cart = document.querySelector('.cart-menu');
   const cartList = document.querySelector('.cart-list');
+  // Simplified
   function clickDropDown(parent,list,otherList) {
+    // Add a click event to the parent element; if clicked, call function()
     parent.addEventListener('click', function () {
+      // Check if the list is open
       if (!list.classList.contains('open')) {
+        // If not, open it, close the other list
         list.classList.add('open');
         otherList.classList.remove('open');
+        // Show the overlay
         document.querySelector('.overlay').style.height = '99vh';
         document.querySelector('.overlay').style.opacity = '0.7';
       }
       else {
+        // Else, close it
         list.classList.remove('open');
+        // Remove the overlay
         document.querySelector('.overlay').style.height = '0';
         document.querySelector('.overlay').style.opacity = '0';
       }
@@ -38,3 +44,12 @@ function topDropDown() {
   clickDropDown(burger,burgerList,cartList);
   clickDropDown(cart,cartList,burgerList);
 }
+
+function start() {
+  var itemCount = 0;
+  // After the window has loaded, call these functions
+  topDropDown();
+  addEmptyList(itemCount);
+}
+
+window.onload = start;
