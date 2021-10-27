d9271297b8c247315e0d2c27a9cc48d5ef204701
KhanhGN <71396471+KhanhGN@users.noreply.github.com>
2021-10-27 11:35:02 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,13 +1,40 @@
-var btn = document.querySelector('.burger-menu');
-var list = document.querySelector('.nav-list');
-var btnOpen = false;
-btn.addEventListener('click', function (T) {
-  if (btnOpen) {
-    list.classList.remove('open');
-    btnOpen = false;
+topDropDown();
+addEmptyList();
+
+function addEmptyList() {
+  var itemCount = 0;
+  const cartList = document.querySelector('.cart-list');
+  if (itemCount == 0) {
+    var emptyText = document.createTextNode('Add something to your cart');
+    var emptyList = document.createElement('li');
+    emptyList.className = 'empty-item';
+    emptyList.appendChild(emptyText);
+    cartList.appendChild(emptyList);
+  } else {
+    document.querySelector('.empty-item').remove();
   }
-  else {
-    list.classList.add('open');
-    btnOpen = true;
-  }
-});
+}
+
+function topDropDown() {
+  const burger = document.querySelector('.burger-menu');
+  const burgerList = document.querySelector('.nav-list');
+  const cart = document.querySelector('.cart-menu');
+  const cartList = document.querySelector('.cart-list');
+  function clickDropDown(parent,list,otherList) {
+    parent.addEventListener('click', function () {
+      if (!list.classList.contains('open')) {
+        list.classList.add('open');
+        otherList.classList.remove('open');
+        document.querySelector('.overlay').style.height = '99vh';
+        document.querySelector('.overlay').style.opacity = '0.7';
+      }
+      else {
+        list.classList.remove('open');
+        document.querySelector('.overlay').style.height = '0';
+        document.querySelector('.overlay').style.opacity = '0';
+      }
+    }
+  )};
+  clickDropDown(burger,burgerList,cartList);
+  clickDropDown(cart,cartList,burgerList);
+}
