e63056569f439a3190737122981f1e6e24e462a9
KhanhGN <71396471+KhanhGN@users.noreply.github.com>
2021-10-29 13:08:05 +0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 // Get all of the required elements
+var cartCount;
 const burger = document.querySelector(".burger-menu");
 const burgerList = document.querySelector(".nav-list");
 const cart = document.querySelector(".cart-menu");
 const cartList = document.querySelector(".cart-list");
 const buy = document.querySelectorAll(".card-buy");
 const card = document.querySelector(".card-container");
-var cartCount;
 const cartContainer = document.querySelector(".cart");
+const card_sort = document.querySelector('.card-sort');
 // If 0 item was loaded for the current user, add an placeholder <li> saying "Add something to your cart"
 function addEmpty() {
   var emptyText = document.createTextNode("Add something to your cart");
@@ -33,6 +34,23 @@ function setStorage(id, count, cartCount) {
 function loadStorage(id) {
   return +localStorage.getItem(id);
 }
+function getTotal() {
+  var total = 0;
+  var totalPrice = document.querySelector('.total');
+  var subTotalList = document.getElementsByClassName("item-subtotal");
+  for (let i = 0; i < subTotalList.length; i++) {
+    total += Number(subTotalList[i].innerHTML);
+  }
+  totalPrice.innerHTML = total.toFixed(2);
+};
+function updateTotal() {
+  document.querySelector(".cart-item-count").innerHTML = cartCount;
+  document.querySelectorAll('.cart-item').forEach(function (item) {
+    console.log(item);
+    item.querySelector('.item-subtotal').innerHTML = (item.querySelector('.quantity').value * item.querySelector('.item-price').innerHTML).toFixed(2);
+    getTotal();
+  });
+}
 function topDropDown() {
   function clickDropDown(parent, list, otherList) {
     // Add a click event to the parent element; if clicked, call function()
@@ -56,10 +74,12 @@ function topDropDown() {
   }
   clickDropDown(burger, burgerList, cartList);
   clickDropDown(cart, cartList, burgerList);
+  // Every time cart is opened, update the subtotal and total
+  cart.addEventListener('click', updateTotal);
 }
 function createContainer(className, id) {
   var container = document.createElement("div");
-  container.className = className
+  container.className = className;
   container.id = id;
   return container;
 }
@@ -71,7 +91,8 @@ function loadItems(file) {
     // Create Image
     var empImg = document.createElement("div");
     empImg.className = "card-img";
-    empImg.style.backgroundImage = "url(\'../img/" + (item.querySelector("ImageURI").innerHTML) + "\')";
+    empImg.style.backgroundImage =
+      "url('../img/" + item.querySelector("ImageURI").innerHTML + "')";
     empCard.appendChild(empImg);
     // Create Card Body
     var empBody = document.createElement("div");
@@ -89,10 +110,16 @@ function loadItems(file) {
     var empFooter = document.createElement("div");
     empFooter.className = "card-footer";
     // Get Price
-    var empPrice = document.createElement("div");
+    var priceContainer = document.createElement("div");
+    var dollarIcon = document.createElement("span");
+    var empPrice = document.createElement("span");
     empPrice.className = "price";
-    empPrice.innerHTML = "$" + item.querySelector("Price").innerHTML;
-    empFooter.appendChild(empPrice);
+    dollarIcon.innerHTML = "$ ";
+    dollarIcon.className = "dollar";
+    empPrice.innerHTML = item.querySelector("Price").innerHTML;
+    priceContainer.appendChild(dollarIcon);
+    priceContainer.appendChild(empPrice);
+    empFooter.appendChild(priceContainer);
     // Create Buy Button
     var empBtn = document.createElement("button");
     empBtn.className = "card-buy";
@@ -101,6 +128,7 @@ function loadItems(file) {
     empIcon.className = "fas fa-cart-plus";
     empBtn.appendChild(empIcon);
     empBtn.addEventListener("click", addToCart);
+    empBtn.addEventListener("click", getTotal);
     empFooter.appendChild(empBtn);
     empCard.appendChild(empFooter);
     // Add card to container
@@ -112,8 +140,8 @@ function loadItems(file) {
   });
 }
 function addToCart() {
-  if (cartCount == 0 && document.querySelector('.empty-item')) {
-    document.querySelector('.empty-item').remove();
+  if (cartCount == 0 && document.querySelector(".empty-item")) {
+    document.querySelector(".empty-item").remove();
   }
   // Set the item to be in cart and lock the add to cart button
   var buyBtn = this;
@@ -140,15 +168,16 @@ function addToCart() {
   // Image
   var cartImg = document.createElement("div");
   cartImg.className = "item-img";
-  cartImg.style.backgroundImage = card.querySelector('.card-img').style.backgroundImage;
+  cartImg.style.backgroundImage =
+    card.querySelector(".card-img").style.backgroundImage;
   cartItem.appendChild(cartImg);
   // Name & Price
   var itemName = document.createElement("h3");
   itemName.className = "item-name";
-  itemName.innerHTML = card.querySelector('h3').innerHTML;
-  var itemPrice = document.createElement("div");
+  itemName.innerHTML = card.querySelector("h3").innerHTML;
+  var itemPrice = document.createElement("span");
   itemPrice.className = "item-price";
-  itemPrice.innerHTML = card.querySelector('.price').innerHTML;
+  itemPrice.innerHTML = card.querySelector(".price").innerHTML;
   cartItem.appendChild(itemName);
   cartItem.appendChild(itemPrice);
   // Minus and Plus button
@@ -156,11 +185,17 @@ function addToCart() {
   itemCount.className = "item-count";
   var minusButton = document.createElement("button");
   minusButton.className = "minus";
-  minusButton.addEventListener('click', function () {
+  minusButton.addEventListener("click", function () {
     // Decrease the counter by 1
-    this.parentNode.querySelector('.quantity').stepDown();
+    this.parentNode.querySelector(".quantity").stepDown();
     // If cartCount is 0, stop reducing
     cartCount = cartCount ? --cartCount : 0;
+    // Update the subtotal and total of cart
+    // This is faster
+    subTotal.innerHTML = (input.value * itemPrice.innerHTML).toFixed(2);
+    // This is slower
+    // updateTotal();
+    getTotal();
     // Update the local storage
     setStorage(card.id, input.value, cartCount);
     // If the item has 0 item, remove it from the cart; add empty if necessary
@@ -173,10 +208,16 @@ function addToCart() {
   });
   var plusButton = document.createElement("button");
   plusButton.className = "plus";
-  plusButton.addEventListener('click', function () {
+  plusButton.addEventListener("click", function () {
     // Increase the counter by 1
-    this.parentNode.querySelector('.quantity').stepUp()
+    this.parentNode.querySelector(".quantity").stepUp();
     cartCount++;
+    // Update the subtotal and total of cart
+    // This is faster
+    subTotal.innerHTML = (input.value * itemPrice.innerHTML).toFixed(2);
+    // This is slower
+    // updateTotal();
+    getTotal();
     // Update the local storage
     setStorage(card.id, input.value, cartCount);
   });
@@ -184,7 +225,7 @@ function addToCart() {
   itemCount.appendChild(input);
   itemCount.appendChild(plusButton);
   cartItem.appendChild(itemCount);
-  //
+  // Item subtotal
   var total = document.createElement("div");
   var subTotal = document.createElement("div");
   subTotal.className = "item-subtotal";
@@ -211,9 +252,8 @@ function start() {
   } else {
     cartCount = +localStorage.getItem("totalCount");
   }
-  checkEmpty()
+  checkEmpty();
   loadFile();
-  document.querySelector(".cart-item-count").innerHTML = cartCount;
   // After the window has loaded, call these functions
   topDropDown();
 }
